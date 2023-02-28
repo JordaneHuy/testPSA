f404441455b92740cef14590c3e5fea425f28e82
HUY Jordane (External) <jordane.huy-ext@kering.com>
2023-02-28 18:03:12 +0100

    prepare network layer

--- psatest/Scenes/HomeView/ViewController.swift
+++ psatest/Scenes/HomeView/ViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 import OpenWeather
 
 class ViewController: UIViewController {
-
+    private let networkService: OpenWeatherQuery = OpenWeatherQuery()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        networkService.requestCity()
         // Do any additional setup after loading the view.
         
-        let popo: Location = Location(name: "popo")
-        popo.test()
+        //let popo: Location = Location(name: "popo")
+        //popo.test()
     }
 
 
